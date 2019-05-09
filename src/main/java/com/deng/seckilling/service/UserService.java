5313acc86ac8960d1eff2a6af3f644b41e2d0a0b
Dengjunbing <647934648@qq.com>
2019-05-09 14:37:15 +0800

    更新user子系统Service

--- src/main/java/com/deng/seckilling/service/UserService.java
+++ src/main/java/com/deng/seckilling/service/UserService.java
@@ -5,15 +5,17 @@ import com.deng.seckilling.constant.Sex;
 import com.deng.seckilling.constant.Status;
 import com.deng.seckilling.dao.UserMapper;
 import com.deng.seckilling.dto.BaseUserInfoDTO;
-import com.deng.seckilling.po.User;
-import com.deng.seckilling.rpc.RpcCommonUtil;
+import com.deng.seckilling.domain.User;
+import com.deng.seckilling.rpc.util.CheckDataUtils;
+import com.deng.seckilling.rpc.util.DataUtils;
+import com.deng.seckilling.rpc.util.EnumUtils;
+import com.deng.seckilling.rpc.util.Md5Utils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,6 +33,26 @@ public class UserService {
     UserMapper userMapper;
 
     /**
+     * 用户基础信息注册service
+     *
+     * @param baseUserInfoDTO 参数实体
+     * @return 注册成功的用户信息
+     */
+    public User registerUserService(BaseUserInfoDTO baseUserInfoDTO) {
+        baseUserInfoDTO.setPassWord(Md5Utils.encryptMd5(baseUserInfoDTO.getPassWord()));
+        User user = new User();
+        DataUtils.entityTransform(baseUserInfoDTO, user);
+        user.setStatus(Status.NORMAL.getValue());
+        user.setRank(EnumUtils.getEnumValueByCode(Rank.class, baseUserInfoDTO.getRank()));
+        user.setSex(EnumUtils.getEnumValueByCode(Sex.class, baseUserInfoDTO.getSex()));
+        int result = userMapper.insertUser(user);
+        if (1 == result) {
+            return user;
+        }
+        return null;
+    }
+
+    /**
      * 验证用户的用户名密码Service
      *
      * @param userName 用户名
@@ -38,8 +60,8 @@ public class UserService {
      * @return 当前用户信息
      */
     public User verifyUserService(String userName, String passWord) {
-        List<User> userList = userMapper.listUser(new User(userName, RpcCommonUtil.encryptMd5(passWord), Status.NORMAL.getValue()));
-        if (RpcCommonUtil.isEmpty(userList)) {
+        List<User> userList = userMapper.listUser(new User(userName, Md5Utils.encryptMd5(passWord), Status.NORMAL.getValue()));
+        if (CheckDataUtils.isEmpty(userList)) {
             return null;
         }
         return userList.get(0);
@@ -52,12 +74,8 @@ public class UserService {
      * @return RpcResponse 查询到的用户集合
      */
     public List<User> queryUsersByConditionService(User user) {
-        user.setPassWord(RpcCommonUtil.encryptMd5(user.getPassWord()));
-        List<User> userList = userMapper.listUser(user);
-        if (RpcCommonUtil.isEmpty(userList)) {
-            return null;
-        }
-        return userList;
+        user.setPassWord(Md5Utils.encryptMd5(user.getPassWord()));
+        return userMapper.listUser(user);
     }
 
     /**
@@ -69,33 +87,8 @@ public class UserService {
      * @return RpcResponse 查询到的用户集合
      */
     public PageInfo<User> queryUsersByConditionService(Integer pageNum, Integer pageSize, User user) {
-        List<User> userList = new ArrayList<User>();
         PageHelper.startPage(pageNum, pageSize);
-        userList = queryUsersByConditionService(user);
-        if (RpcCommonUtil.isEmpty(userList)) {
-            return null;
-        }
-        return new PageInfo<User>(userList);
-    }
-
-    /**
-     * 用户基础信息注册service
-     *
-     * @param baseUserInfoDTO 参数实体
-     * @return 注册成功的用户信息
-     */
-    public User registerUserService(BaseUserInfoDTO baseUserInfoDTO) {
-        baseUserInfoDTO.setPassWord(RpcCommonUtil.encryptMd5(baseUserInfoDTO.getPassWord()));
-        User user = new User();
-        RpcCommonUtil.entityTransform(baseUserInfoDTO, user);
-        user.setStatus(Status.NORMAL.getValue());
-        user.setRank(RpcCommonUtil.getEnumValueByCode(Rank.class, baseUserInfoDTO.getRank()));
-        user.setSex(RpcCommonUtil.getEnumValueByCode(Sex.class, baseUserInfoDTO.getSex()));
-        int result = userMapper.insertUser(user);
-        if (1 == result) {
-            return user;
-        }
-        return null;
+        return new PageInfo<User>(queryUsersByConditionService(user));
     }
 
     /**
@@ -105,36 +98,18 @@ public class UserService {
      * @return 完善成功的用户信息
      */
     public User completeUserInfoService(User user) {
-        if (false == isExist(user.getId())) {
+        List<User> userList = queryUsersByConditionService(new User(user.getId()));
+        if (CheckDataUtils.isEmpty(userList)) {
             log.warn("===>id:{} not exist", user.getId());
             return null;
         }
-        if (RpcCommonUtil.isEmpty(user.getPassWord())) {
-            user.setPassWord(RpcCommonUtil.encryptMd5(user.getPassWord()));
+        if (false == CheckDataUtils.isEmpty(user.getPassWord())) {
+            user.setPassWord(Md5Utils.encryptMd5(user.getPassWord()));
         }
+        user.setUserName(null);
         user.setRank(null);
         user.setStatus(null);
-        int result = userMapper.updateUser(user);
-        if (1 == result) {
-            return queryUsersByConditionService(new User(user.getId())).get(0);
-        }
-        return null;
-    }
-
-    /**
-     * 根据ID作废用户账户Service
-     *
-     * @param id 待作废的用户账户Id
-     * @return 作废成功的用户账户Id
-     */
-    public Long invalidUserByIdService(Long id) {
-        User user = new User(id);
-        user.setStatus(RpcCommonUtil.getEnumValueByCode(Status.class, 3));
-        int result = userMapper.updateUser(user);
-        if (1 == result) {
-            return id;
-        }
-        return null;
+        return 1 == userMapper.updateUser(user) ? queryUsersByConditionService(new User(user.getId())).get(0) : null;
     }
 
     /**
@@ -144,17 +119,9 @@ public class UserService {
      * @return 冻结成功的用户Id
      */
     public Long frozenUserByIdService(Long id) {
-        if (false == isExist(id)) {
-            log.warn("===>id:{} not exist", id);
-            return null;
-        }
         User user = new User(id);
-        user.setStatus(RpcCommonUtil.getEnumValueByCode(Status.class, 2));
-        int result = userMapper.updateUser(user);
-        if (1 == result) {
-            return id;
-        }
-        return null;
+        user.setStatus(Status.FROZEN.getValue());
+        return 1 == userMapper.updateUser(user) ? id : null;
     }
 
     /**
@@ -165,59 +132,34 @@ public class UserService {
      * @return 冻结成功后的用户Id
      */
     public Long unfrozenUserByIdService(Long id) {
-        if (false == isExist(id)) {
-            log.warn("===>id:{} not exist", id);
-            return null;
-        }
-        List<User> userList = queryUsersByConditionService(new User(id));
-        if (RpcCommonUtil.isEmpty(userList)) {
-            return null;
-        }
-        String status = RpcCommonUtil.getEnumValueByCode(Status.class, 2);
-        if (userList.get(0).getStatus().equals(status)) {
-            log.warn("===>user status is not frozen");
-            return null;
-        }
         User user = new User(id);
-        user.setStatus(RpcCommonUtil.getEnumValueByCode(Status.class, 1));
-        int result = userMapper.updateUser(user);
-        if (1 == result) {
-            return id;
-        }
-        return null;
+        user.setStatus(Status.NORMAL.getValue());
+        return 1 == userMapper.updateUser(user) ? id : null;
     }
 
     /**
-     * 私有方法：判断ID是否存在
+     * 根据ID作废用户账户Service
      *
-     * @param id 用户Id
-     * @return boolean:是否存在
+     * @param id 待作废的用户账户Id
+     * @return 作废成功的用户账户Id
      */
-    public boolean isExist(Long id) {
-        if (RpcCommonUtil.isEmpty(id)) {
-            return false;
-        }
-        List<User> userList = queryUsersByConditionService(new User(id));
-        if (RpcCommonUtil.isEmpty(userList)) {
-            return false;
-        }
-        return true;
+    public Long invalidUserByIdService(Long id) {
+        User user = new User(id);
+        user.setStatus(Status.INVALID.getValue());
+        return 1 == userMapper.updateUser(user) ? id : null;
     }
 
     /**
-     * 私有方法：判断name是否存在
+     * 判断name是否存在
      *
      * @param name 用户name
      * @return boolean:是否存在
      */
     public boolean isExist(String name) {
-        if (RpcCommonUtil.isEmpty(name)) {
+        if (CheckDataUtils.isEmpty(name)) {
             return true;
         }
         List<User> userList = queryUsersByConditionService(new User(name));
-        if (RpcCommonUtil.isEmpty(userList)) {
-            return false;
-        }
-        return true;
+        return false == CheckDataUtils.isEmpty(userList);
     }
 }
