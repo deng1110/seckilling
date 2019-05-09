71fa8004951402bff5b557c0066d79d247db9c19
Dengjunbing <647934648@qq.com>
2019-05-09 14:44:28 +0800

    更新ErrorCode

--- src/main/java/com/deng/seckilling/constant/ErrorCode.java
+++ src/main/java/com/deng/seckilling/constant/ErrorCode.java
@@ -16,17 +16,13 @@ public class ErrorCode {
     public static final ErrorInfo SECKILLING_PARAMS_ERROR = new ErrorInfo(1001, "入参不能为空或格式错误");
     public static final ErrorInfo USERLOGIN_FAIL_ERROR = new ErrorInfo(1002, "用户名或者密码错误");
     public static final ErrorInfo QUERYUSER_NULL_ERROR = new ErrorInfo(1003, "查询成功，但查出零条数据");
-    public static final ErrorInfo REGISTER_FAIL_ERROR = new ErrorInfo(1004, "注册失败");
     public static final ErrorInfo USERNAME_EXIT_ERROR = new ErrorInfo(1005, "用户名已存在");
     public static final ErrorInfo USER_NOTEXIT_ERROR = new ErrorInfo(1006, "该用户不存在");
     public static final ErrorInfo COMPLETE_USERINFO_ERROR = new ErrorInfo(1007, "完善个人信息失败");
-    public static final ErrorInfo QUERYUSER_FAIL_ERROR = new ErrorInfo(1008, "查询用户失败");
     public static final ErrorInfo INVALID_USER_ERROR = new ErrorInfo(1009, "作废用户账户失败");
     public static final ErrorInfo FROZEN_USER_ERROR = new ErrorInfo(1010, "冻结用户账户失败");
-    public static final ErrorInfo FENYE_PARAMS_ERROR = new ErrorInfo(1011, "请输入正确页码");
-    public static final ErrorInfo PASSWORD_NOTPARAMS_ERROR = new ErrorInfo(1012, "密码不能作为查询条件");
-    public static final ErrorInfo QUERYPARAMS_ISNULL_ERROR = new ErrorInfo(1013, "查询条件不能为空");
-    public static final ErrorInfo USERSTATUS_NORMAL_ERROR = new ErrorInfo(1014, "该用户当前为非冻结状态，无需解冻");
+    public static final ErrorInfo NOTNORMALUSER_CANNOT_FROZEN = new ErrorInfo(1011, "用户账户状态非正常不允许冻结");
+    public static final ErrorInfo NOTFROZENUSER_CANNOT_UNFROXEN = new ErrorInfo(1012, "用户账户之前非冻结状态不允许解冻");
     public static final ErrorInfo UNFROZEN_USER_ERROR = new ErrorInfo(1015, "解冻用户失败");
 
 }
